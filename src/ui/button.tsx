eeb46105672e03a65d600d2d887d3e2605a00302
flynno <luke.laz.flynn@gmail.com>
2025-01-19 21:18:09 -0700

    updated secondary button css

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -15,11 +15,11 @@ export default function Button({
     <button
       {...rest}
       className={clsx(
-        `flex h-10 items-center justify-center rounded-lg px-4 align-middle text-sm font-medium outline-none transition-colors focus-visible:ring-4 aria-disabled:cursor-not-allowed aria-disabled:opacity-50`,
+        `flex h-10 w-full items-center justify-center rounded-lg px-4 align-middle text-sm font-medium outline-none transition-colors focus-visible:ring-4 aria-disabled:cursor-not-allowed aria-disabled:opacity-50`,
         {
           "bg-blue-500 text-white hover:bg-blue-400 active:bg-blue-600":
             isPrimary,
-          "acive:border-gray-500 border border-black bg-white text-black hover:border-gray-400 hover:bg-gray-400 hover:text-white active:bg-gray-500 active:text-white":
+          "acive:border-gray-500 border border-gray-500 bg-white text-black hover:border-gray-200 hover:bg-gray-200 active:bg-gray-300":
             !isPrimary,
         },
         className,
