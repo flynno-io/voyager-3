31101a00f3219a558e748f561b5de0593c8d2e3b
flynno <luke.laz.flynn@gmail.com>
2025-01-21 15:36:02 -0700

    created fetch api to get data for blog posts

--- src/app/transmissions/layout.tsx
+++ src/app/transmissions/layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className="w-full flex-none md:w-64">
           <SideNav />
         </div>
-        <div className="flex-grow p-6 md:overflow-y-auto md:p-4">
+        <div className="m-6 flex-grow md:m-4 md:overflow-y-auto">
           {children}
         </div>
       </div>
