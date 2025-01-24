e131b0ccc3ccd7d877d7dbe6f6e02de5b1db2314
flynno <luke.laz.flynn@gmail.com>
2025-01-23 21:36:37 -0700

    fixed logout redirect on sidenav

--- src/ui/transmissions/sidenav.tsx
+++ src/ui/transmissions/sidenav.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link";
+import Link from "next/link"
+import { logout } from "@/lib/actions/auth"
 // import BlogPostsLinks from "@/ui/blog-posts";
 
 // TODO: update to work with my application.
@@ -14,12 +15,12 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         {/* <BlogPostsLinks /> */}
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
+        <form action={logout}>
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <div className="hidden md:block">Sign Out</div>
           </button>
         </form>
       </div>
     </div>
-  );
+  )
 }
